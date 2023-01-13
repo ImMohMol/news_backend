07bbfb75c0dc9088fdc70acc728b90bde762e615
ImMohMol <mhoseinmola1380@gmail.com>
2023-01-13 22:25:44 +0330

    Improvment: 1. User is now an implementation of UserDetails interface

--- src/main/java/com/news/news/model/User.java
+++ src/main/java/com/news/news/model/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @AllArgsConstructor
@@ -11,7 +16,8 @@ import lombok.*;
 @Getter
 @Setter
 @Builder
-public class User {
+@Table(name = "tb_user")
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -27,4 +33,29 @@ public class User {
     @Min(value = 6, message = "Password is too short!")
     @Max(value = 100, message = "Password is too long!")
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
