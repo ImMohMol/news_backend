a502d1c235075523ebef187bf10df91cb37586f8
ImMohMol <mhoseinmola1380@gmail.com>
2023-01-16 11:46:43 +0330

    Improvment: 1. News model created 2. NewCategory class created

---
+++ src/main/java/com/news/news/model/News.java
@@ -0,0 +1,25 @@
+package com.news.news.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_news")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String body;
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NewsCategory category;
+    private Long numberOfVisits = 0L;
+}
---
+++ src/main/java/com/news/news/model/NewsCategory.java
@@ -0,0 +1,5 @@
+package com.news.news.model;
+
+public enum NewsCategory {
+    POLICY, SECURITY, SPORT, TECHNOLOGY, WORLD
+}
