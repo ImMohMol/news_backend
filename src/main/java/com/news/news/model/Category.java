486a578e8165a87db939c1f6eace8451cad4051b
ImMohMol <mhoseinmola1380@gmail.com>
2023-01-17 13:51:52 +0330

    Improvment: 1. category name is unique now and is not nullable any more

--- src/main/java/com/news/news/model/Category.java
+++ src/main/java/com/news/news/model/Category.java
@@ -16,6 +16,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column(unique = true, nullable = false)
     private String name;
     @OneToOne()
     private News news;
